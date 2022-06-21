84fa250131a6e69c0d2d9bbedc2dd3fd52549bde
taoufik ahjam <taoufikahjam@gmail.com>
2022-06-21 21:49:07 +0200

    refactoring the code

--- app.js
+++ app.js
@@ -19,23 +19,17 @@ app.get('/api/courses', (req, res) => {
 
 //Method Get course by id
 app.get('/api/courses/:id', (req, res) => {
-  const course = courses.find((c) => {
-    return c.id === parseInt(req.params.id);
-  });
-  if (!course) res.status(404).send('course not found');
-  res.send(course);
+  //Look if course Exist
+  const course=ifExist(req,res);
+  if (course)res.send(course);
 });
+
 
 //Method Post add course
 app.post('/api/courses', (req, res) => {
-  const schema = Joi.object({
-    name: Joi.string().min(3).required(),
-  });
-  const { error, value } = schema.validate({ name: req.body.name });
-  if (error) {
-    res.status(400).send(error.details[0].message);
-    return;
-  }
+
+//Validate input data
+validate(req, res)
   const course = {
     id: courses.length + 1,
     name: req.body.name,
@@ -43,33 +37,43 @@ app.post('/api/courses', (req, res) => {
   courses.push(course);
   res.send(course);
 });
- 
 
 //Method Put update course
 app.put('/api/courses/:id', (req, res) => {
   //Look if course Exist
-  const course = courses.find((c) => {
-    return c.id === parseInt(req.params.id);
-  });
-  if (!course) res.status(404).send('course not found');
+  const course=ifExist(req,res);
 
   //Validate input data
+  validate(req, res)
+
+  //Update course
+  course.name = req.body.name;
+  res.send(course);
+});
+
+//Validate input data
+function validate(req,res){
   const schema = Joi.object({
     name: Joi.string().min(3).required(),
   });
-
   const { error, value } = schema.validate({ name: req.body.name });
   if (error) {
     res.status(400).send(error.details[0].message);
     return;
   }
-  //Update course
-   course.name=value.name
-   res.send(course)
-   
+}
 
 
-});
+ //Look if course Exist
+function ifExist(req,res){
+  const course = courses.find((c) => {
+    return c.id === parseInt(req.params.id);
+  });
+  if (!course) res.status(404).send('course not found');
+   return course;
+}
+
+
 
 //Listen Server
 const port = process.env.PORT || 3000;
