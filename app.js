4e9ce8c9bb26596cdea35ccc7d9279e8c7c48ff1
taoufik ahjam <taoufikahjam@gmail.com>
2022-06-21 17:44:44 +0200

    add express with sipmle exemple

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
-_ = require('underscore')
+const express= require('express')
+const app=express()
 
-//exemple (for other functions , consult : https://underscorejs.org/)
-const res1=_.map([1, 2, 3], function(num){ return num * 3; });
+app.get('/',function(req,res){
+    res.send('Home page')
 
-console.log(res1);
+})
+app.get('/api',function(req,res){
+    res.send([
+        {id:1,name:'ahmed'},
+        {id:2,name:'ali'},
+        {id:3,name:'mohamed'},
+    ])
+
+})
+
+app.listen(3000,function(){
+    console.log('listen at port 3000');
+})
+
+
+
