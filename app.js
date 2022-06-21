1dd05bb298a56cd99777f2cbeee2c6f22944ced8
taoufik ahjam <taoufikahjam@gmail.com>
2022-06-21 12:44:34 +0200

    event argument

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ import EventEmetter from "events";
 
 const eventInstance=new EventEmetter();
 
-eventInstance.on('event1',function(){
-    console.log('event1 called');
+eventInstance.on('event1',function(arg){
+    console.log('event1 called',arg);
 })
 
-eventInstance.emit('event1')
+eventInstance.emit('event1',{id:1,message:"salam"})
