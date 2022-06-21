19e480bec09e5a000c5711a16794a79cc5c76f8b
taoufik ahjam <taoufikahjam@gmail.com>
2022-06-21 17:50:45 +0200

    install nodemon

--- app.js
+++ app.js
@@ -1,22 +1,14 @@
-const express= require('express')
-const app=express()
+const express = require('express');
+const app = express();
 
-app.get('/',function(req,res){
-    res.send('Home page')
+app.get('/', (_, res) => res.send('Home page'));
 
-})
-app.get('/api',function(req,res){
-    res.send([
-        {id:1,name:'ahmed'},
-        {id:2,name:'ali'},
-        {id:3,name:'mohamed'},
-    ])
+app.get('/api', (_, res) =>
+  res.send([
+    { id: 1, name: 'ahmed' },
+    { id: 2, name: 'ali' },
+    { id: 3, name: 'mohamed' },
+  ])
+);
 
-})
-
-app.listen(3000,function(){
-    console.log('listen at port 3000');
-})
-
-
-
+app.listen(3000, () => console.log('listen at port 3000'));
