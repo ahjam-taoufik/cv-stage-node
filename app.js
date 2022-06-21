5c0e1995cc7e213ca8994d129cc1c186b947d27c
taoufik ahjam <taoufikahjam@gmail.com>
2022-06-21 18:49:50 +0200

    http get request

--- app.js
+++ app.js
@@ -1,26 +1,31 @@
 const express = require('express');
 const app = express();
 
+const courses = [
+  { id: 1, name: 'courses1' },
+  { id: 2, name: 'courses2' },
+  { id: 3, name: 'courses3' },
+];
+
 app.get('/', (_, res) => res.send('Home page'));
 
+app.get('/api/courses', (req, res) => {
+  res.send(courses);
+});
 
-// app.get('/api/c/:id', (req, res) =>
-//   // res.send(req.params)
-//      res.send(req.params.id)
-// );
+app.get('/api/courses/:id', (req, res) => {
+   //method 1
+   // const course=courses[req.params.id-1];
+   //   if(!course) res.status(404).send('course not found')
+   //   res.send(course)
+   
+   //method 2
+  const course = courses.find((c) => {return c.id === parseInt(req.params.id)});
+    if(!course) res.status(404).send('course not found')
+    res.send(course)
+  
+  
+});
 
-
-// app.get('/api/c/:id/:years/:month', (req, res) =>
-//   // res.send(req.params)
-//      res.send(req.params.month)
-// );
-
-app.get('/api/c/:id', (req, res) =>//exemple in url http://localhost:3000/api/c/1?sortBy=name
-      res.send(req.query)          //exemple in url http://localhost:3000/api/c/1?sortBy=name&orderBy=id
-);
-
-
-
-const port=process.env.PORT || 3000;
-
+const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`listen at port ${port}`));
