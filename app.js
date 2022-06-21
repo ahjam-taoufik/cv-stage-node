d494e42db06b008a49a1be2a2a56b9bd1ab8a03a
taoufik ahjam <taoufikahjam@gmail.com>
2022-06-21 18:14:21 +0200

    params and query

--- app.js
+++ app.js
@@ -3,13 +3,23 @@ const app = express();
 
 app.get('/', (_, res) => res.send('Home page'));
 
-app.get('/api', (_, res) =>
-  res.send([
-    { id: 1, name: 'ahmed' },
-    { id: 2, name: 'ali' },
-    { id: 3, name: 'mohamed' },
-  ])
+
+// app.get('/api/c/:id', (req, res) =>
+//   // res.send(req.params)
+//      res.send(req.params.id)
+// );
+
+
+// app.get('/api/c/:id/:years/:month', (req, res) =>
+//   // res.send(req.params)
+//      res.send(req.params.month)
+// );
+
+app.get('/api/c/:id', (req, res) =>//exemple in url http://localhost:3000/api/c/1?sortBy=name
+      res.send(req.query)          //exemple in url http://localhost:3000/api/c/1?sortBy=name&orderBy=id
 );
+
+
 
 const port=process.env.PORT || 3000;
 
