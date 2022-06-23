4ff069d06918043ae1e015e1681753194391ffe8
taoufik ahjam <taoufikahjam@gmail.com>
2022-06-23 19:07:37 +0200

    Referencing Documents

--- app.js
+++ app.js
@@ -1,112 +1,54 @@
 const mongoose = require('mongoose');
 
-const delay = (n) => {
-  return new Promise(function (resolve) {
-    setTimeout(resolve, n * 1000);
-  });
-};
+mongoose.connect('mongodb://localhost/playground1')
+  .then(() => console.log('Connected to MongoDB...'))
+  .catch(err => console.error('Could not connect to MongoDB...', err));
 
-mongoose
-  .connect('mongodb://localhost/playground')
-  .then(() => console.log('you are Connected to DB .....'))
-  .catch((err) => console.log('Failed to connect', err));
+const Author = mongoose.model('Author', new mongoose.Schema({
+  name: String,
+  bio: String,
+  website: String
+}));
 
-const courseSchema = mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    // lowercase: true,
-    // uppercase:true
-    // match: /patern/
-    // maxLength: 20,
-    // minLength: 4,
-  },
+const Course = mongoose.model('Course', new mongoose.Schema({
+  name: String,
   author:{
-    type:String,
-    required: true,
-    minLength:4
-  },
+    type:mongoose.Schema.Types.ObjectId,
+    ref:'Author'
+  }
+}));
 
-  tags: {
-    type: Array,
-    validate: {
-      // isAsync: true,
-      validator:async function (v) {
-          await delay(0)
-          //this work in async
-          return val = v && v.length > 0;
-  
-       
-      },
-      message: 'the course should be at least one tage',
-    },
-  },
-
-  category: {
-    type: String,
-    enum: ['Angular', 'React'],
-  },
-  date: { type: Date, default: Date.now },
-  isPublished: Boolean,
-  price: {
-    type: Number,
-    required: function () {
-      return this.isPublished;
-    },
-    min: 10,
-    max: 100,
-    set:v=>Math.round(v),//modif value from double to integer  and persist it in DB 
-    get:v=>Math.round(v) // get value from DB and modif it to integer
-  },
-});
-
-const Course = mongoose.model('Course_db', courseSchema);
-
-async function createCourse() {
-  const course = new Course({
-    name: 'Learn React',
-    author: 'ali',
-     tags: ["Angula","React"],
-    isPublished: true,
-    price: 9,
+async function createAuthor(name, bio, website) { 
+  const author = new Author({
+    name, 
+    bio, 
+    website 
   });
 
-  try {
-    const result = await course.save();
-    console.log(result);
-  } catch (err) {
-    for(error in err.errors){
-      // console.log(error);
-       console.log(err.errors[error].message);
-    }
-  }
-}
-
-async function getData() {
-  const data = await Course.find();
-
-  console.log(data);
-}
-
-async function updatecourse(id) {
-  const result = await Course.findByIdAndUpdate(
-    { _id: id },
-    {
-      $set: {
-        isPublished: true,
-        author: 'taoufik3',
-      },
-    },
-    { new: true }
-  );
+  const result = await author.save();
   console.log(result);
 }
 
-async function deleteCourse(id) {
-  //if course not existe , this method return null
-  const result = await Course.findByIdAndRemove({ _id: id });
-
+async function createCourse(name, author) {
+  const course = new Course({
+    name, 
+    author
+  }); 
+  
+  const result = await course.save();
   console.log(result);
 }
 
-createCourse();
+async function listCourses() { 
+  const courses = await Course
+    .find()
+    .select('name');
+  console.log(courses);
+}
+
+// createAuthor('Taoufik', 'My bio', 'My Website');
+
+// createCourse('Node Course', 'authorId')
+createCourse('Node Course', '62b49c8311939ebd5c901523')
+
+// listCourses();
